201379c2f7857012c0e15e6025cac57382d240f9
ayeshani <ayeshanikaushalya@gmail.com>
2020-10-08 15:00:29 +0530

    --- Auto-Git Commit ---

--- profile.js
+++ profile.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
+                $(".profile-pic").attr("src", e.target.result);
             }
     
             reader.readAsDataURL(input.files[0]);
@@ -14,11 +14,11 @@ $(document).ready(function() {
     }
     
 
-    $(".file-upload").on('change', function(){
+    $(".file-upload").on("change", function(){
         readURL(this);
     });
     
-    $(".upload-button").on('click', function() {
+    $(".upload-button").on("click ", function() {
        $(".file-upload").click();
     });
 });
