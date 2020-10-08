4b6887f054a9ba77939525ec5bf0f8fb0ed6def0
ayeshani <ayeshanikaushalya@gmail.com>
2020-10-08 13:30:07 +0530

    --- Auto-Git Commit ---

--- user_profile.js
+++ user_profile.js
@@ -1,4 +1,4 @@
-$('#signup').click(function() {
+/*$('#signup').click(function() {
     $('.pinkbox').css('transform', 'translateX(80%)');
     $('.signin').addClass('nodisplay');
     $('.signup').removeClass('nodisplay');
@@ -9,9 +9,19 @@ $('#signup').click(function() {
     $('.signup').addClass('nodisplay');
     $('.signin').removeClass('nodisplay');
   });
-
+*/
 
   function upmyfunction(){
-      document.getElementById("pinkbox");
-      
+      document.getElementById("pinkbox").css('transform', 'translateX(80%)');
+      document.getElementById("signin").addClass('nodisplay');
+      document.getElementById("signup").addClass('nodisplay');
+
   }
+
+  function inmyfunction(){
+    document.getElementById("pinkbox").css('transform', 'translateX(0%)');
+    document.getElementById("signup").addClass('nodisplay');
+    document.getElementById("signin").addClass('nodisplay');
+
+}
+
